c241ece745d5b3e7141313c1ef2f784bed85d72f
Pavel Surov <surov_p@tanukitech.ru>
2023-05-10 19:12:46 +0300

    homework 5 - В validate_request_json заменил аннотацию с str на dict - В validate_status_code заменил в ассерте 201 на status_code

--- dm_api_account/utilities.py
+++ dm_api_account/utilities.py
@@ -2,7 +2,7 @@ import requests
 from pydantic import BaseModel
 
 
-def validate_request_json(json: str | BaseModel):
+def validate_request_json(json: dict | BaseModel):
     if isinstance(json, dict):
         return json
     return json.dict(by_alias=True, exclude_none=True)
@@ -10,4 +10,4 @@ def validate_request_json(json: str | BaseModel):
 
 def validate_status_code(response: requests.Response, status_code: int):
     assert response.status_code == status_code, \
-                f'Статус код ответа должен быть равен 201, но он равен {response.status_code}'
+                f'Статус код ответа должен быть равен {status_code}, но он равен {response.status_code}'
