a8bc05caa798f0198ddf8ac5c8e015cf66031392
Pavel Surov <surov_p@tanukitech.ru>
2023-06-10 12:15:12 +0500

    переход на сгенерированнный апи клиент

--- generic/helpers/account.py
+++ generic/helpers/account.py
@@ -1,31 +1,27 @@
 from dm_api_account.models import Registration
-try:
-    from services.dm_api_account import Facade
-except ImportError:
-    ...
 
 
 class Account:
     def __init__(self, facade):
-        self.facade = facade
+        from services.dm_api_account import Facade
+        self.facade: Facade = facade
 
     def set_headers(self, headers):
         self.facade.account_api.client.session.headers.update(headers)
 
     def register_new_user(self, login: str, email: str, password: str, status_code: int):
-        response = self.facade.account_api.post_v1_account(
-            json=Registration(
+        response = self.facade.account_api.register(
+            registration=Registration(
                 login=login,
                 email=email,
                 password=password,
             ),
-            status_code=status_code
         )
         return response
 
     def activate_registered_user(self, login: str):
         token = self.facade.mailhog.get_token_by_login(login=login)
-        response = self.facade.account_api.put_v1_account_token(
+        response = self.facade.account_api.activate(
             token=token
         )
         print('Получили такой токен:', token)
--- generic/helpers/login.py
+++ generic/helpers/login.py
@@ -4,18 +4,19 @@ from dm_api_account.models import LoginCredentials
 
 class Login:
     def __init__(self, facade):
-        self.facade = facade
+        from services.dm_api_account import Facade
+        self.facade: Facade = facade
 
     def set_headers(self, headers):
         self.facade.login_api.client.session.headers.update(headers)
 
     def login_user(self, login: str, password: str, remember_me: bool = True):
         with allure.step("Авторизация пользователя"):
-            response = self.facade.login_api.post_v1_account_login(
-                json=LoginCredentials(
+            response = self.facade.login_api.v1_account_login_post(
+                login_credentials=LoginCredentials(
                     login=login,
                     password=password,
-                    rememberMe=remember_me
+                    remember_me=remember_me
                 )
             )
         return response
@@ -29,10 +30,10 @@ class Login:
         return token
 
     def logout_user(self, **kwargs):
-        response = self.facade.login_api.delete_v1_account_login(**kwargs)
+        response = self.facade.login_api.v1_account_login_delete(**kwargs)
         return response
 
     def logout_user_from_all_devices(self, **kwargs):
-        response = self.facade.login_api.delete_v1_account_login_all(**kwargs)
+        response = self.facade.login_api.v1_account_login_all_delete(**kwargs)
         return response
 
