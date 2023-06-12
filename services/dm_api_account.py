57eac44ca749fc774c3bd2c037fbbd653dbfd6f6
Pavel Surov <surov_p@tanukitech.ru>
2023-06-12 19:40:04 +0500

    поправил requirements.txt

--- services/dm_api_account.py
+++ services/dm_api_account.py
@@ -1,5 +1,5 @@
-from dm_api_account.apis import AccountApi
-from dm_api_account.apis import LoginApi
+from dm_api_account.apis.account_api import AccountApi
+from dm_api_account.apis.login_api import LoginApi
 from generic.helpers.account import Account
 from generic.helpers.login import Login
 from dm_api_account import Configuration, ApiClient
