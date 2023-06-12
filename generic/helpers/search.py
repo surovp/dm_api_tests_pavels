d128f5dbf326e2856ba10c7419971db111c58ad8
Pavel Surov <surov_p@tanukitech.ru>
2023-06-12 19:20:00 +0500

    поправил атрибут класса

--- generic/helpers/search.py
+++ generic/helpers/search.py
@@ -3,7 +3,7 @@ from dm_api_search.search_pb2 import SearchRequest
 
 
 class Search:
-    def init(self, target):
+    def __init__(self, target):
         self.grpc_search = DmApiSearch(target=target)
 
     def search(self, query: str, size: int, skip: int, search_across: list):
